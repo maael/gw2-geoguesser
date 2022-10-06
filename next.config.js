0e0855d7e269051f86bcf44512c6fd9c4cbb6ea2
Matt Elphick <matt.a.elphy@gmail.com>
2022-10-07 00:31:18 +0100

    feat: Use that_shaman based tiles, add basic layout

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   images: {
-    domains: ['render.guildwars2.com', 'static.staticwars.com'],
+    domains: ['render.guildwars2.com', 'static.staticwars.com', 'gw2-sightseeing-app.s3.us-west-2.amazonaws.com'],
   },
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,19 @@ const Map = dynamic(() => import('~/components/primitives/Map'), {
 export default function Index() {
   return (
     <div suppressHydrationWarning className="bg-purple-300 text-purple-800 flex justify-center items-center">
-      <Map />
+      <div className="flex flex-row bg-green-200 w-full h-full justify-center items-center">
+        <div className="h-4/5 w-full bg-slate-500"></div>
+      </div>
+      <div className="flex flex-col absolute top-10 right-0 bg-gray-300 pl-10 pr-20 py-2 text-xl rounded-l-full">
+        <div>Round: 1/5</div>
+        <div>Score: 0</div>
+      </div>
+      <div className="absolute bottom-10 right-10 w-1/2 h-3/5 scale-50 hover:scale-100 origin-bottom-right transition-all opacity-50 hover:opacity-100">
+        <div className="isolate w-full h-full">
+          <Map />
+        </div>
+        <button className="absolute left-1/2 bottom-5 bg-red-200 z-50 px-3 py-2">Guess</button>
+      </div>
     </div>
   )
 }
