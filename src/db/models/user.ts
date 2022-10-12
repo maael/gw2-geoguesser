abb2279c0cc204ece1a6b0422937fbebeeab24fa
Matt Elphick <matt.a.elphy@gmail.com>
2022-10-12 10:55:00 +0100

    fix: Use bcryptjs instead of bcrypt

--- src/db/models/user.ts
+++ src/db/models/user.ts
@@ -1,5 +1,5 @@
 import { Schema, Model } from 'mongoose'
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 import { connect } from '../mongo'
 import { WithDoc, User } from '../../types'
 
